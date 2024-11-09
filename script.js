ec5131547f3bd33e7ba089ec444c7e1df8db2a50
NadicaJordanova <nadicajordanova3@gmail.com>
2024-11-09 17:25:26 +0100

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+var number = 50
+
+if (number % 3 == 0 && number % 5 == 0 )
+    {
+        console.log("BuzzFizz")
+    }
+else if (number % 3 == 0){
+    console.log("Fizz")
+}
+else if(number % 5 == 0)
+{
+    console.log("Buzz")
+}
+
